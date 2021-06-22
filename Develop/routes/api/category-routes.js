ff9b70759a57f0bfd7c86edac6d4881987edd50e
AliMitchell <ali.mitchell.n@gmail.com>
2021-06-22 08:51:57 -0600

    env file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -30,18 +30,71 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock']
+      }
+    ]
+  })
+    .then(dbCategoryData => {
+      if(!dbCategoryData){
+        res.status(404).json({message: 'No categories in this ID'});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: RegExp.body.category_name
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData){
+      res.status(404).json({message: 'No category found with this ID'});
+      return;
+    }
+    res.json(dbCategoryData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id:RegExp.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({message: 'No category found with this ID'});
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.delete
 });
 
 module.exports = router;
